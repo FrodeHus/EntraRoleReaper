030f0e639602dd380988c99561645526b4f1300a
Frode Hus <frode.hus@outlook.com>
2025-08-24 13:52:11 +0200

    Add generic Repository class for data access
    Introduces a new `Repository<TEntity>` class for common data access operations, including add, retrieve, delete, and update functionalities using Entity Framework Core. The class supports filtering, ordering, and including related entities in queries.
    
    Also adds `TargetResourceRepository` to manage `TargetResource` and `TargetResourceProperty` entities, initializing repository instances and providing an asynchronous save method.

---
+++ server/Data/Repositories/TargetResourceRepository.cs
@@ -0,0 +1,11 @@
+﻿using EntraRoleReaper.Api.Data.Models;
+
+namespace EntraRoleReaper.Api.Data.Repositories;
+
+public class TargetResourceRepository(ReaperDbContext context)
+{
+    public Repository<TargetResource> TargetResources = new(context);
+    public Repository<TargetResourceProperty> TargetResourceProperties = new(context);
+
+    public Task Save() => context.SaveChangesAsync();
+}
